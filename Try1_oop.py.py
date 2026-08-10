9b575f70fa560d192dbcd38d270eae84fa81c08f
Fariz Akbar <farizakbar1222@gmail.com>
2026-08-10 19:01:05 +0700

    Add files via upload

---
+++ Try1_oop.py.py
@@ -0,0 +1,12 @@
+nama= "Fariz Akbar"
+hobi= "Tidur"
+umur= 16
+lakilaki= "true"
+BeratBadan= 58,5
+
+print("====Biodata====")
+print("nama:",nama)
+print("hobi:",hobi)
+print("umur:",umur,"Tahun")
+print("laki laki:",lakilaki)
+print("Berat badan:",BeratBadan)
---
+++ Tugas1_oop.py.py
@@ -0,0 +1,18 @@
+roti = 100
+
+print("persediaan awal:",roti)
+
+roti += 50
+print("setelah 1 hari bertambah 50:",roti)
+
+roti *= 3
+print("jumlah produksi dikalikan 3:",roti)
+
+roti -=120
+print("sebanyak 120 roti di kirim ke pelanggan:",roti)
+
+roti /=4
+print("sisa roti di bagi ke 4 etalase:",roti)
+
+roti +=10
+print("setiap etalase mendapatkan 10 roti tambahan:",roti)
