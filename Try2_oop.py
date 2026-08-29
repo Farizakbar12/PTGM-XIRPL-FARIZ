309dfcb12b8c258bf6cb5ce20dd04973df2fa446
Fariz Akbar <farizakbar1222@gmail.com>
2026-08-29 04:05:47 -0700

    Rename Try2_oop.py.py to Try2_oop.py

---
+++ Try2_oop.py
@@ -0,0 +1,5 @@
+kopi = 15000
+roko = 30000
+roti = 14000
+
+print("total yang harus di bayar :",kopi+roko+roti,)
--- Try2_oop.py.py
+++
@@ -1,5 +0,0 @@
-kopi = 15000
-roko = 30000
-roti = 14000
-
-print("total yang harus di bayar :",kopi+roko+roti,)
